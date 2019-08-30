2eca1ab26b9a0e96cdcf1e5844baf019ab493c8d
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-08-30 19:43:37 -0400

    initial migration completed without errors

--- data/migrations/20190830192232_create-tables.js
+++ data/migrations/20190830192232_create-tables.js
@@ -1,8 +1,47 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('projects', tbl => {
+      tbl.increments();
+      tbl.string('project_name', 128).notNullable();
+      tbl.string('project_description');
+      tbl.boolean('completed', 128).defaultTo(false);
+    })
+    .createTable('tasks', tbl => {
+      tbl.increments();
+      tbl
+        .integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('projects')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      tbl.string('task_name', 128).notNullable();
+      tbl.string('task_description', 128).notNullable();
+      tbl.string('task_notes', 128);
+      tbl.boolean('completed').defaultTo(false);
+    })
+    .createTable('resources', tbl => {
+      tbl.increments();
+      tbl
+        .integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('projects')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      tbl
+        .string('resource_name', 128)
+        .unique()
+        .notNullable();
+      tbl.string('description', 128);
+    });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('projects')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources');
 };
