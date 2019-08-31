b0739e928f9dd67fa957536ecbec3df9b0c4e573
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-08-30 20:44:59 -0400

    boilerplated routes

--- models/business-router.js
+++ models/business-router.js
@@ -4,4 +4,18 @@ const Data = require('./helpers');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {});
+
+router.get('/', (req, res) => {});
+
+router.get('/', (req, res) => {});
+
+router.post('/', (req, res) => {});
+
+router.post('/', (req, res) => {});
+
+router.post('/', (req, res) => {});
+
+function convertBool(req, res, next) {}
+
 module.exports = router;
---
+++ models/helpers.js
@@ -0,0 +1,22 @@
+const db = require('../data/db-config.js');
+
+module.exports = {
+  getProjects,
+  getResources,
+  getTasks,
+  addProject,
+  addResource,
+  addTask
+};
+
+function getProjects() {}
+
+function getResources() {}
+
+function getTasks() {}
+
+function addProject() {}
+
+function addResource() {}
+
+function addTask() {}
